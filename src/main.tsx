08c95745f06c80f0dcbee13ed0f3ca866d4d3364
Isaques Estúdios <isaquesestudios@gmail.com>
2026-02-01 23:13:47 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// src/main.tsx completo
 import { ViteReactSSG } from "vite-react-ssg";
 import { App, routes } from "./App";
 import "./index.css";
@@ -7,10 +8,10 @@ export const createRoot = ViteReactSSG(
     routes,
     rootContainer: true 
   },
-  // O parâmetro 'helmetContext' é injetado pelo Vite durante o processo de build
   ({ helmetContext }) => {
     return {
-      wrapper: (props) => <App {...props} helmetContext={helmetContext} />
+      // É vital passar o helmetContext aqui para o wrapper
+      wrapper: (props: any) => <App {...props} helmetContext={helmetContext} />
     };
   }
 );
